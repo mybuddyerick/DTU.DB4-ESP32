4aff490b307cb2a1de9d6526d34acbefdcd66617
Eirik Daniel's <mybuddyerick.dev@gmail.com>
2026-06-11 15:00:21 +0200

    added csv as file type

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ MIME_TYPES = {
     ".html": "text/html; charset=utf-8",
     ".css": "text/css; charset=utf-8",
     ".js": "application/javascript; charset=utf-8",
+    ".csv": "text/csv; charset=utf-8",
+    ".json": "application/json; charset=utf-8",
+    ".txt": "text/plain; charset=utf-8",
+
     ".jpg": "image/jpeg",
     ".jpeg": "image/jpeg",
     ".png": "image/png",
     ".svg": "image/svg+xml",
     ".ico": "image/x-icon",
-    ".txt": "text/plain; charset=utf-8",
 }
 
-BLOCKED = {
+BLOCKED_ROOT_FILES = {
     "boot.py",
     "main.py",
     "pymakr.conf",
