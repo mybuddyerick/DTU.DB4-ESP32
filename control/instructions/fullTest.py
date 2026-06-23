865a87936e0c6fc56daa7027a298bace12be486f
Arka <arkaghosh0106@gmail.com>
2026-06-23 15:55:30 +0200

    updated get_status to update global dictionary

--- control/instructions/fullTest.py
+++ control/instructions/fullTest.py
@@ -8,6 +8,8 @@ from control.services.feeding import Feeding
 from control.services.websocket_server import WebSocketServer
 from control.services.thermal_pid import Thermal_PID
 
+import time
+
 
 oled = None
 scheduler = None
@@ -75,40 +77,8 @@ def step_wait():
 def get_status():
     #rgb_values = _safe_get_latest(rgb_sensor)
     #temp_values = _safe_get_latest(temperature)
-
     #cooler_state = _cooler_running(pumps)
     #waste_state = _waste_running(pumps)
     #peltier_state = peltier.running()
-
-    dummy_status = {
-        "available": True,
-        "uptime_ms": 1,
-        "system": {"free_memory": 100000},
-        "rgb": {
-            "available": True,
-            "sensor_enabled": True,
-            "logging": True,
-            "time_ms": 1,
-            "clear": 1000,
-            "red": 1,
-            "green": 255,
-            "blue": 100
-        },
-        "temperature": {
-            "available": True,
-            "sensor_enabled": True,
-            "logging": True,
-            "time_ms": 1,
-            "temp_c": 25.5,
-            "raw_average": 2000,
-            "voltage": 1.5,
-            "resistance": 10000
-        },
-        "outputs": {
-            "water_pump": False,
-            "spray_pump": False,
-            "peltier": False
-        }
-    }
-
-    return dummy_status
+    SYSTEM_STATE["uptime_ms"] = time.ticks_ms()
+    return SYSTEM_STATE
