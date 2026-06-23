a35e6867d088c8808a6d322a719c17e235ebdf06
Arka <arkaghosh0106@gmail.com>
2026-06-23 15:25:57 +0200

    fixed merge conflicty

---
+++ control/helpers/system_state.py
@@ -0,0 +1,30 @@
+SYSTEM_STATE = {
+    "available": True,
+    "uptime_ms": 1,
+    "system": {"free_memory": 100000},
+    "rgb": {
+        "available": True,
+        "sensor_enabled": True,
+        "logging": True,
+        "time_ms": 1,
+        "clear": 1000,
+        "red": 1,
+        "green": 255,
+        "blue": 100
+    },
+    "temperature": {
+        "available": True,
+        "sensor_enabled": True,
+        "logging": True,
+        "time_ms": 1,
+        "temp_c": 25.5,
+        "raw_average": 2000,
+        "voltage": 1.5,
+        "resistance": 10000
+    },
+    "outputs": {
+        "water_pump": False,
+        "spray_pump": False,
+        "peltier": False
+    }
+}
